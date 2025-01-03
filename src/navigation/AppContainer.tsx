a5605912d34c5f6d5d6191312cc1c384e20a35b5
Christopher Batin <christopher.batin@and.digital>
2025-01-03 10:02:37 +0000

    feat: Tab bar when enabled (#95)

--- src/navigation/AppContainer.tsx
+++ src/navigation/AppContainer.tsx
@@ -10,6 +10,7 @@ import {checkAppIntegrity} from '../util/FirebaseUtils/FirebaseUtils';
 import RNExitApp from 'react-native-exit-app';
 import AlertMessage from '../components/atoms/AlertMessage/AlertMessage';
 import BootSplash from 'react-native-bootsplash';
+import {screensLoaded} from '../state/reducers/SplashScreenReducer';
 
 function AppContainer(): React.JSX.Element {
   const {
@@ -39,6 +40,7 @@ function AppContainer(): React.JSX.Element {
       if (isValidApp) {
         dispatch(fetchRemoteConfig());
         dispatch(fetchLondonTime());
+        dispatch(screensLoaded(true));
       }
     };
 
