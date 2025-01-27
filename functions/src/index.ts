9b4226900502f6d11ba3ea94f7f036be998107b7
Jake-Sturgeon <sturgeonjake52@gmail.com>
2025-01-27 11:06:58 +0000

    address comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,36 +13,29 @@ declare global {
   }
 }
 
+const config = {region: 'europe-west1', maxInstances: 1, timeoutSeconds: 60};
+
 /**
  * Desk Booking function
  * This instance of the firebase function handles only desk booking and designed to run requests sequencially and on one instance at a time
  * NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/deskBooking)
  */
-exports.deskapi = onRequest(
-  {region: 'europe-west1', maxInstances: 1, timeoutSeconds: 60},
-  App,
-);
+exports.deskapi = onRequest(config, App);
 /**
  * Car Booking function
  * This instance of the firebase function handles only car booking and designed to run requests sequencially and on one instance at a time
  * NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/carBooking)
  */
-exports.carapi = onRequest(
-  {region: 'europe-west1', maxInstances: 1, timeoutSeconds: 60},
-  App,
-);
+exports.carapi = onRequest(config, App);
 
 /**
  * Utils function
  *  NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/utils)
  */
-exports.api = onRequest(
-  {region: 'europe-west1', maxInstances: 1, timeoutSeconds: 60},
-  App,
-);
+exports.api = onRequest(config, App);
 
 /**
  * Scheduled Function - This is run every day so we can call an end point at 9pm to check car spaces are allocated.
