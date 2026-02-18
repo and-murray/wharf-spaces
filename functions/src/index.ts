f4817069d63defb04a2d3b32bd27b2704147b3a3
Christopher Batin <chris@batin.me>
2026-02-18 14:24:31 +0000

    feat: adds gen1 functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import {onRequest} from 'firebase-functions/v2/https';
 import {onSchedule} from 'firebase-functions/v2/scheduler';
-
+import * as functions from 'firebase-functions';
 import App from './App';
 import {DecodedIdToken} from 'firebase-admin/auth';
 import {callCarAPI} from './v1/Services/Scheduled/callCarAPI';
@@ -21,13 +21,34 @@ const config = {region: 'europe-west1', maxInstances: 1, timeoutSeconds: 60};
  * NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/deskBooking)
  */
+// GEN 1 Function to be removed after migration
+exports.deskapi = functions
+  .region('europe-west1')
+  .runWith({
+    // Only allow one booking to be made at a time
+    maxInstances: 1,
+    timeoutSeconds: 60,
+  })
+  .https.onRequest(App);
+// GEN 2 Function to stay after migration
 exports.deskapi = onRequest(config, App);
+
 /**
  * Car Booking function
  * This instance of the firebase function handles only car booking and designed to run requests sequencially and on one instance at a time
  * NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/carBooking)
  */
+// GEN 1 Function to be removed after migration
+exports.carapi = functions
+  .region('europe-west1')
+  .runWith({
+    // Only allow one booking to be made at a time
+    maxInstances: 1,
+    timeoutSeconds: 60,
+  })
+  .https.onRequest(App);
+// GEN 2 Function to stay after migration
 exports.carapi = onRequest(config, App);
 
 /**
@@ -35,6 +56,14 @@ exports.carapi = onRequest(config, App);
  *  NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/utils)
  */
+// GEN 1 Function to be removed after migration
+exports.api = functions
+  .region('europe-west1')
+  .runWith({
+    timeoutSeconds: 60,
+  })
+  .https.onRequest(App);
+// GEN 2 Function to stay after migration
 exports.api = onRequest(config, App);
 
 /**
@@ -42,6 +71,24 @@ exports.api = onRequest(config, App);
  *  NOTE: Please do not change the function name as the firebase uses the firebase to
  * auto generate http URL i.e. base_url/region/function-name (e.g: baseurl/europe-west1/utils)
  */
+// GEN 1 Function to be removed after migration
+exports.scheduledFunctionCrontab = functions
+  .region('europe-west1')
+  .runWith({
+    timeoutSeconds: 240,
+  })
+  .pubsub.schedule('0 21 * * *')
+  .timeZone('Europe/London')
+  .onRun(async () => {
+    try {
+      await callCarAPI();
+      console.log('Called car API Successfully');
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  });
+// GEN 2 Function to stay after migration
 exports.scheduledFunctionCrontab = onSchedule(
   {
     schedule: '0 21 * * *',
