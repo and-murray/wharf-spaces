1793584757268d74901f65c8c52dfdb8eb3f02b6
Christopher Batin <chris@batin.me>
2026-02-19 16:30:46 +0000

    feat: updated defaults

--- src/types/Endpoints.ts
+++ src/types/Endpoints.ts
@@ -8,17 +8,15 @@ export function endpointsDefault(): Endpoints {
   // TODO: Emulators
   if (__DEV__) {
     return {
-      carAPIURL: 'https://carapigen2-qg3ssmjwca-ew.a.run.app/v1/booking',
-      deskAPIURL: 'https://deskapigen2-qg3ssmjwca-ew.a.run.app/v1/booking',
+      carAPIURL: 'https://carapigen2-qg3ssmjwca-ew.a.run.app',
+      deskAPIURL: 'https://deskapigen2-qg3ssmjwca-ew.a.run.app',
       genericAPIURL: 'https://apigen2-qg3ssmjwca-ew.a.run.app',
     };
   }
   // TODO: Change to Prod Gen2 when we know them
   return {
-    carAPIURL:
-      'https://europe-west1-murray-apps.cloudfunctions.net/carapi/v1/booking',
-    deskAPIURL:
-      'https://europe-west1-murray-apps.cloudfunctions.net/deskapi/v1/booking',
+    carAPIURL: 'https://europe-west1-murray-apps.cloudfunctions.net/carapi',
+    deskAPIURL: 'https://europe-west1-murray-apps.cloudfunctions.net/deskapi',
     genericAPIURL: 'https://europe-west1-murray-apps.cloudfunctions.net/api',
   };
 }
