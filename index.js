4c590559885134101dee75526e0425d744fe05d5
edwinsimpson <102525507+edwinsimpson@users.noreply.github.com>
2026-02-26 14:36:38 +0000

    Bug: miscellaneous fixes (#124)
    * updates deprecated Layout for Linear Transition on animated view
    
    * strips outlineWidth from View and TextInput on android

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import { Platform, View, TextInput } from 'react-native';
+
+// TODO: remove this android logic when we migrate from native-base to gluestack
+if (Platform.OS === 'android') {
+  const fixOutlineProps = (Component) => {
+    const originalRender = Component.render;
+    Component.render = function (props, ref) {
+      if (props?.style) {
+        const style = Array.isArray(props.style)
+          ? props.style.flat().filter(Boolean)
+          : { ...props.style };
+
+        const stripInvalidOutline = (styles) => {
+          if (styles && typeof styles.outlineWidth === 'string') {
+            delete styles.outlineWidth;
+          }
+        };
+
+        if (Array.isArray(style)) {
+          style.forEach(stripInvalidOutline);
+        } else {
+          stripInvalidOutline(style);
+        }
+      }
+      return originalRender.call(this, props, ref);
+    };
+  };
+
+  // Apply fix to the core components NativeBase wraps
+  fixOutlineProps(View);
+  fixOutlineProps(TextInput);
+}
 
 // Register background handler
 messaging().setBackgroundMessageHandler(() => {});
