19bca3b4dc9b10996c3c76392aad01931fbb0f38
Christopher Batin <chris@batin.me>
2026-02-26 21:46:31 +0000

    feat: get google org data

--- functions/src/v1/Controllers/user.create.controller.ts
+++ functions/src/v1/Controllers/user.create.controller.ts
@@ -3,6 +3,8 @@ import type {Request, Response} from 'express';
 import {CollectionName} from '../Services/CollectionName';
 import {User, Role, BusinessUnit} from '../Models/booking.model';
 import { UserRecord } from 'firebase-admin/auth';
+import { refreshToken } from 'firebase-admin/app';
+import {google} from 'googleapis';
 
 export const createUser = async (req: Request, res: Response) => {
     try {
@@ -10,13 +12,18 @@ export const createUser = async (req: Request, res: Response) => {
         console.log(req.user);
         const id = req.user?.uid;
         if (id) {
+            console.log('======== GET FIRESTORE USER 1');
             let firestoreUser = await getFirestoreUser(id);
             if (firestoreUser) {
                 res.status(200).send(firestoreUser);
             } else {
+                console.log('======== GET AUTH USER');
                 const user = await admin.auth().getUser(id);
                 if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
                     const token = req.headers.authorization.split('Bearer ')[1];
+                    console.log('======== CREATE FIRESTORE USER');
+                    console.log('==== TOKEN ');
+                    console.log(refreshToken);
                     firestoreUser = await createFirestoreUser(user, token);
                     await setUserDoc(firestoreUser);
                 } else {
@@ -53,15 +60,21 @@ async function createFirestoreUser(
   let organisationData;
   let nameData;
   try {
-    organisationData = await getOrganisationData(accessToken);
+    console.log('============================================== PROVIDER DATA');
+    console.log(firebaseUser.providerData);
+    organisationData = await getOrganisationData(firebaseUser.providerData[0].uid);
+    console.log('====== GOT ORG DATA');
+    console.log(organisationData);
     nameData = await getNameData(accessToken);
+    console.log('====== GET NAME DATA');
+    console.log(nameData);
   } catch (error) {
     throw new Error('Error fetching organisational unit');
   }
   if (organisationData && nameData) {
-    let department =
-      organisationData.organizations?.[0]?.department?.toLowerCase() ??
-      'unknown';
+    let department = BusinessUnit.Enum.murray as string;
+     // organisationData.organizations?.[0]?.department?.toLowerCase() ??
+     // 'unknown';
     if ((department as string).includes('tenzing')) {
       department = BusinessUnit.Enum.tenzing;
     } else if ((department as string).includes('adams')) {
@@ -81,7 +94,7 @@ async function createFirestoreUser(
         email: email,
         profilePicUrl: firebaseUser.photoURL ?? '',
         role: Role.Enum.user,
-        businessUnit: businessUnit,
+        businessUnit: businessUnit as BusinessUnit,
         createdAt: admin.firestore.Timestamp.now(),
         updatedAt: admin.firestore.Timestamp.now(),
       };
@@ -107,22 +120,71 @@ const setUserDoc = async (user: User): Promise<User> => {
 };
 export default createUser;
 
-const getOrganisationData = async (accessToken: string) => {
-  const organisationResponse = await fetch(
-    'https://people.googleapis.com/v1/people/me?personFields=organizations',
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    },
-  );
-  if (!organisationResponse.ok) {
-    throw new Error(
-      `API request failed with status ${organisationResponse.status}`,
-    );
+const SCOPES = [
+  'https://www.googleapis.com/auth/contacts',
+  'https://www.googleapis.com/auth/contacts.readonly',
+  'https://www.googleapis.com/auth/directory.readonly',
+  'https://www.googleapis.com/auth/user.addresses.read',
+  'https://www.googleapis.com/auth/user.birthday.read',
+  'https://www.googleapis.com/auth/user.emails.read',
+  'https://www.googleapis.com/auth/user.gender.read',
+  'https://www.googleapis.com/auth/user.organization.read',
+  'https://www.googleapis.com/auth/user.phonenumbers.read',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/cloud-platform',
+];
+// The path to the credentials file.
+// const filePath = require('/Users/christopher.batin/developer/wharf-spaces/murray-apps-dev-firebase-adminsdk-57gqb-953020e9a4.json');
+
+const getOrganisationData = async (googleId: string) => {
+  console.log('=============');
+  console.log('ORG DATA START');
+  try {
+    console.log('GOOGLE ID');
+    console.log(googleId);
+
+  //   const auth = await authenticate({
+  //   scopes: SCOPES,
+  //   keyfilePath: filePath,
+  // });
+    const auth = new google.auth.GoogleAuth({
+      keyFile: '/Users/christopher.batin/developer/wharf-spaces/murray-apps-dev-firebase-adminsdk-57gqb-953020e9a4.json',
+      scopes: SCOPES,
+    });
+    const service = google.people({version: 'v1', auth});
+    const personData = await service.people.get({
+      resourceName: `people/${googleId}`,
+      personFields: 'organizations',
+    });
+    console.log(`===== PERSON DATA ${JSON.stringify(personData)}`);
+    return personData;
+  } catch (error) {
+    console.log('======= ERROR');
+    console.error(error);
+    return null;
   }
-  return organisationResponse.json();
+  // try {
+  //   const organisationResponse = await fetch(
+  //     'https://people.googleapis.com/v1/people/me?personFields=organizations',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     },
+  //   );
+  //   if (!organisationResponse.ok) {
+  //     console.log('THIS API ERROR');
+  //     console.error(JSON.stringify(organisationResponse));
+  //     throw new Error(
+  //       `API request failed with status ${organisationResponse.status}`,
+  //     );
+  //   }
+  //   return organisationResponse.json();
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 
 const getNameData = async (accessToken: string) => {
@@ -141,12 +203,17 @@ const getNameData = async (accessToken: string) => {
 };
 
 async function getFirestoreUser(uid: string): Promise<User | undefined> {
+  console.log('DB');
   const db = admin.firestore();
+  console.log(`UID ${uid}`);
   const ref = db.collection(CollectionName.users);
   const userDoc = await ref.doc(uid).get();
-  if (userDoc) {
+  console.log('===== USER DOC RETRIEVED');
+  if (userDoc.exists) {
+    console.log('===== USER DOC EXISTS');
     return User.parse(userDoc.data());
   } else {
+    console.log('===== USER DOC DOES NOT EXISTS');
     return undefined;
   }
 }
